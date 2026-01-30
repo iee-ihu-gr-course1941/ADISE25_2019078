7cdabddc1fe0d98b219d65c82da10556579b5dce
Koumpos <man.koumpos@gmail.com>
2026-01-30 22:17:01 +0200

    Update db.php

--- lib/db.php
+++ lib/db.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/db_upass.php';   // ΜΟΝΟ αυτό το extra αρχείο
-
 function db(): PDO {
     static $pdo = null;
     if ($pdo !== null) {
@@ -43,6 +41,7 @@ function db(): PDO {
 
     return $pdo;
 }
+
 
 
 
